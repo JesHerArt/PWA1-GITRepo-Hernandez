4da1ac904e56d588b7b4fd6ecea13cfce058edd6
Jessica Hernandez <jesherart@fullsail.edu>
2014-10-03 00:45:50 -0400

    Added heading to the javascript file before forgetting. Continued to add more comments to each line of code.

--- Wk1/goal1_assign_duel1 FINAL/js/main.js
+++ Wk1/goal1_assign_duel1 FINAL/js/main.js
@@ -1,24 +1,29 @@
-
+/*
+ Name: Jessica Hernandez
+ Date: October 02, 2014
+ Class & Section:  PWA1 - 2014-01
+ Assignment: Homework - Analyze Duel #1
+ */
 
 // self-executing function
-(function(){        //Creating a self-executing function
+(function(){        //Creating a self-executing function.
 
     console.log("FIGHT!!!");        //Log to the console the chant of "FIGHT!!" when the program initiates.
 
     //player name
-    var playerOneName = "Spiderman";        //Declare and define the player 1 name.
-    var playerTwoName = "Batman";           //Declare and define the player 2 name.
+    var playerOneName = "Spiderman";        //Declare and define the player 1 name. Global variable.
+    var playerTwoName = "Batman";           //Declare and define the player 2 name. Global variable.
 
     //player damage
-    var player1Damage = 20;         //Declare and define the amount of damage player 1 inflicts.
-    var player2Damage = 20;         //Declare and define the amount of damage player 2 inflicts.
+    var player1Damage = 20;         //Declare and define the amount of damage player 1 inflicts. Global variable.
+    var player2Damage = 20;         //Declare and define the amount of damage player 2 inflicts. Global variable.
 
     //player health
-    var playerOneHealth = 100;          //Declare and define player 1's max health.
-    var playerTwoHealth = 100;          //Declare and define player 2's max health.
+    var playerOneHealth = 100;          //Declare and define player 1's max health. Global variable.
+    var playerTwoHealth = 100;          //Declare and define player 2's max health. Global variable.
 
     //initiate round
-    var round=0;            //Declare and deinfe the initial value of round to zero.
+    var round=0;            //Declare and define the initial value of round to zero. Global variable.
 
     function fight(){
         console.log ("Inside the fight function");
@@ -34,26 +39,26 @@
             var f2 = Math.floor(Math.random()*(player2Damage-minDamage2)+minDamage2);
 
             //inflict damage
-            playerOneHealth-=f1;
-            playerTwoHealth-=f2;
+            playerOneHealth-=f1;        //Reduce and update player 1's health (global variable) by the value of the f1 variable result.
+            playerTwoHealth-=f2;        //Reduce and update player 2's health (global variable) by the value of the f2 variable result.
 
             console.log(playerOneName+": "+playerOneHealth + " " + playerTwoName+":"+playerTwoHealth);
 
             //check for victor
             var result = winnerCheck();
-            console.log(result);
+            console.log(result);            //Logging the current result to the console output.
             if (result==="no winner")
             {
                 round++;
                 alert(playerOneName+":"+playerOneHealth+"  *ROUND "+round+" OVER"+"*  "+playerTwoName+":"+playerTwoHealth);
 
-            } else{
-                alert(result);
-                break;
-            };
+            } else{                 //End of the if statement and begin of the else statement.
+                alert(result);      //Alerting the user of the current result.
+                break;              //Force the loop to break/stop when the condition results in else.
+            };                      //End of the else statement.
 
-          };
-    };
+        };                          //End of the for loop.
+    };                              //End of the fight function.
 
     function winnerCheck(){
         console.log ("Inside the winnerCheck function");
@@ -67,7 +72,7 @@
         {
             result = playerOneName+" WINS!!!"
         };
-       return result;
+        return result;
     };
 
     /*******  The program gets started below *******/
