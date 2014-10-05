e4ab440e365b39b3daa3b32a32313a87eea4ffa8
Jessica Hernandez <jesherart@fullsail.edu>
2014-10-05 18:11:48 -0400

    Added a header and comments to the database.js file.
    Added a header and began to fix syntax errors in the search.js file.

--- Wk1/goal3_debug/js/database.js
+++ Wk1/goal3_debug/js/database.js
@@ -1,23 +1,30 @@
-var db = [
-	"JavaScript Version History|http://http://wddbs.com/javascript/sfw/online/#p=videos&s=3&file=01.html",
-	"JavaScript in the Browser|http://wddbs.com/javascript/sfw/online/#p=videos&s=3&file=01.html",
-	"Script Tag|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"JavaScript Syntax|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Data Type Literals|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Strings & HTML Strings|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Numbers|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Arrays|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Objects|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Functions|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Scope|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Loops|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Conditionals|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Typeof|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Timers|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Native Objects|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",
-	"Debugging Tools|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=03.html",
-	"Firebug|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=03.html",
-	"Error Types|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=03.html",
-	"Closures|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=04.html",
-	"Context|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=04.html"
-];
+/*
+ Name: Jessica Hernandez
+ Date: October 05, 2014
+ Class & Section:  PWA1 - 2014-01
+ Assignment: Homework - Analyze Buggy Search v1
+ */
+
+var db = [      //Declare and define the variable as an Array, where all indexes are going to have strings.
+	"JavaScript Version History|http://http://wddbs.com/javascript/sfw/online/#p=videos&s=3&file=01.html",      //index 0
+	"JavaScript in the Browser|http://wddbs.com/javascript/sfw/online/#p=videos&s=3&file=01.html",              //index 1
+	"Script Tag|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                             //index 2
+	"JavaScript Syntax|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                      //index 3
+	"Data Type Literals|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                     //index 4
+	"Strings & HTML Strings|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                 //index 5
+	"Numbers|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                                //index 6
+	"Arrays|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                                 //index 7
+	"Objects|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                                //index 8
+	"Functions|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                              //index 9
+	"Scope|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                                  //index 10
+	"Loops|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                                  //index 11
+	"Conditionals|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                           //index 12
+	"Typeof|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                                 //index 13
+	"Timers|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                                 //index 14
+	"Native Objects|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=02.html",                         //index 15
+	"Debugging Tools|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=03.html",                        //index 16
+	"Firebug|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=03.html",                                //index 17
+	"Error Types|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=03.html",                            //index 18
+	"Closures|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=04.html",                               //index 19
+	"Context|http://wddbs.com/javascript/sfw/online/#p=videos&s=1&file=04.html"                                 //index 20
+];      //End of db Array variable declaration/definition.
--- Wk1/goal3_debug/js/search.js
+++ Wk1/goal3_debug/js/search.js
@@ -1,74 +1,84 @@
+/*
+ Name: Jessica Hernandez
+ Date: October 05, 2014
+ Class & Section:  PWA1 - 2014-01
+ Assignment: Homework - Analyze Buggy Search v1
+ */
+
 // Create privatized scope using a self-executing function
 (function(){
-	
+
 	// Variable initialization (DO NOT FIX ANY OF THE BELOW VAR's)
 	var resultsDIV = document.getElementById("results"),
 		searchInput = document.forms[0].search,
-		currentSearch = ''
-	;
-	
+		currentSearch = '';
+
 	// Validates search query
-	var validqte == function(query){
-		
+	var validqte = function(query){
+
 		// Trim whitespace from start and end of search query
-		while(query.charAt(0) = " "){
+		while (query.charAt(0) = " "){
 			query = query.substring(1, query.length);
-		};
-		while(query.charAt(query.length-1) === ""){
-			query = query.substring(0, query.length-1);
-		;
-		
+		}
+
+		while (query.charAt(query.length-1) === "") {
+            query = query.substring(0, query.length - 1);
+        }
+
 		// Check search length, must have 3 characters
-		if(query.length < 3){
-			alert("Your search query is too small, try again.);
-			
+		if (query.length < 3){
+			alert("Your search query is too small, try again.");
+
 			// (DO NOT FIX THE LINE DIRECTLY BELOW)
 			searchInput.focus();
 			return;
-		};
-		
+		}
+
 		search(query);
 	};
-	
+
 	// Finds search matches
-	var search = function(query)
-		
+	var search = function(query){
+
 		// split the user's search query string into an array
 		var queryArray = query.join(" ");
-		
+
 		// array to store matched results from database.js
 		var results = [];
 
 		// loop through each index of db array
-		for(var i=0, j=db.length; i<j; i++){
-		
-			// each db[i] is a single video item, each title ends with a pipe "|"
-			// save a lowercase variable of the video title
-			var dbTitleEnd = db[i].indexOf('|');
-			var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
-			
-			// loop through the user's search query words
-			// save a lowercase variable of the search keyword
-			for(var ii=0, jj=queryArray.length; ii<jj; ii++){
-				var qitem = queryArray[ii].tolowercase();
-				
-				// is the keyword anywhere in the video title?
-				// If a match is found, push full db[i] into results array
-				var compare = dbitem.indexOf(qitem);
-				if(compare !== -1){
-					results.push(db[i]);
-				};
-			;
-		;
+		for(var i=0, j=db.length; i<j; i++) {
+
+            // each db[i] is a single video item, each title ends with a pipe "|"
+            // save a lowercase variable of the video title
+            var dbTitleEnd = db[i].indexOf('|');
+            var dbitem = db[i].tolowercase().substring(0, dbTitleEnd);
+
+            // loop through the user's search query words
+            // save a lowercase variable of the search keyword
+            for (var ii = 0, jj = queryArray.length; ii < jj; ii++) {
+                var qitem = queryArray[ii].tolowercase();
+
+                // is the keyword anywhere in the video title?
+                // If a match is found, push full db[i] into results array
+                var compare = dbitem.indexOf(qitem);
+                if (compare !== -1) {
+                    results.push(db[i]);
+                }
+
+            }
+        }
 		
 		results.sort();
 		
 		// Check that matches were found, and run output functions
 		if(results.length = 0){
 			noMatch();
-		}else{
+
+		} else{
 			showMatches(results);
-		};
+		}
+
 	};
 	
 	// Put "No Results" message into page (DO NOT FIX THE HTML VAR NOR THE innerHTML)
@@ -102,19 +112,21 @@
 			
 			// make the video link - THE NEXT LINE IS CORRECT.
 			html += '<p><a href=' + url + '>' + title + '</a></p>';
-		};
+		}
+
 		resultsDIV.innerHTML = html; //THIS LINE IS CORRECT.
+
 	};
 	
 	// The onsubmit event will be reviewed in upcoming Course Material.
 	// THE LINE DIRECTLY BELOW IS CORRECT
 	document.forms[0].onsubmit = function(){
-		var query = searchInput.value;
-		validqte(query);
+        var query = searchInput.value;
+        validqte(query);
 
         // return false is needed for most events - this will be reviewed in upcoming course material
         // THE LINE DIRECTLY BELOW IS CORRECT
-		return false;
-	;
+        return false;
+    }
 
-})();
+}();
