eb145bf614147853b304e32ed43dec4d8d57667c
Jessica Hernandez <jesherart@fullsail.edu>
2014-10-17 14:52:52 -0400

    Adding the final version of the flowchart for the search engine v3 program.

--- Wk3/goal4_assign_guessing_game/js/main.js
+++ Wk3/goal4_assign_guessing_game/js/main.js
@@ -1,14 +1,16 @@
 /**
  * GUESSING GAME:
  *
- * Created By: 
- * Date: 
+ * Created By: Jessica Hernandez
+ * Date: October 16, 2014
  * 
  * GUESSING GAME
  */
 
 //Game variables
-(function ()
+(function (){
+
+
 
 
 })();
